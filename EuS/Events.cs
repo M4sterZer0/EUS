59c2a7e0a321003a114109df5cac9d32ad314145
M4sterZer0 <masterzero@tuta.io>
2017-01-08 22:08:11 +0100

    Rewrite
    Rewrite vom alten Script

---
+++ EuS/Events.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace EuS
+{
+    public class Events : Script
+    {
+        internal void onResourceStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void OnClientEvent(Client sender, string eventName, object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void OnClientConnected(Client player)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void OnPlayerFinishedDownload(Client player)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void OnClientDisconnected(Client player, string reason)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EuS/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+using Datenbank;
+
+namespace EuS
+{
+    public class EUS : Script
+    {
+        // Datenbank
+        Db db = new Db();
+        public EUS()
+        {
+            var Events = new Events();
+            API.onResourceStart += Events.onResourceStart;
+            API.onClientEventTrigger += Events.OnClientEvent;
+            API.onPlayerConnected += Events.OnClientConnected;
+            API.onPlayerFinishedDownload += Events.OnPlayerFinishedDownload;
+            API.onPlayerDisconnected += Events.OnClientDisconnected;
+        }
+    }
+}
